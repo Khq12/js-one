dd2399e6511f4b153fc95648d8e5d4a9b2c975be
Khq12 <149036289+Khq12@users.noreply.github.com>
2024-03-09 23:11:18 +0500

    first

---
+++ first.js
@@ -0,0 +1,25 @@
+var name=prompt("Enter your name:");
+var age=prompt("Enter your age:");
+var firstProblem=prompt("Solve: 12+3 =");
+var secondProblem=prompt("Solve: 45-34 =");
+var thirdProblem=prompt("Solve 56*8 =");
+var fourthProblem=prompt("Solve 90/4 =");
+var fifthProblem=prompt("Solve 34 % 4 =");
+
+alert("Open the console!");
+
+console.log("Problem 1: 12+3 =", 12+3, ", your answer: ", firstProblem);
+console.log("Problem 2: 45-34 =", 45-34, ", your answer: ", secondProblem);
+console.log("Problem 3: 56*8 =", 56*8, ", your answer: ", thirdProblem);
+console.log("Problem 4: 90/4 =", 90/4, ", your answer: ", fourthProblem);
+console.log("Problem 5: 34 % 4 =", 34 % 4, ", your answer: ", fifthProblem);
+
+
+
+
+var x=+prompt("Enter the first number: ");
+var y=+prompt("Enter the second number: ");
+var z=+prompt("Enter the third number: ");
+var avg=(x+y+z)/3;
+
+console.log(avg);
